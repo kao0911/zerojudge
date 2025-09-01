19ead0938a8089158f3215a708bcbf4d442ea3fb
kao0911 <xuan47987@gmail.com>
2025-09-01 11:03:44 +0800

    a003

---
+++ python/a003.py
@@ -0,0 +1,12 @@
+try:
+    while True:
+        M, D = map(int, input().split())
+        S=(M*2+D)%3
+        if S==0:
+            print("普通")
+        elif S==1:
+            print("吉")
+        else:
+            print("大吉")
+except EOFError:
+    pass
