163a98d186eb8a2a9fb25564d43e85dc79c36e72
kao0911 <xuan47987@gmail.com>
2025-09-03 09:18:54 +0800

    a004

---
+++ python/a004.py
@@ -0,0 +1,9 @@
+try:
+    while True:
+        a = int(input())
+        if a % 4 == 0 and a % 100 != 0 or a % 400 == 0:
+            print("閏年")
+        else:
+            print("平年")
+except EOFError:
+    pass
