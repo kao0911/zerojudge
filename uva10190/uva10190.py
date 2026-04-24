ccab102c9ea68fde802359af2c24c0b8079f8add
kao0911 <xuan47987@gmail.com>
2026-04-24 17:51:37 +0800

    0424

--- uva10190/uva10190.py
+++ uva10190/uva10190.py
@@ -122,4 +122,45 @@ while True:
     else:
         print(*my_numbers)
 '''
+'''
+while True:
+    try:
+        n, m = map(int, input().split())
+        status = True
+        a = [n]
+            
+        while n > 1:
+            # print(f'{ n // m = }')
+            if m == 1 or m == 0:
+                status = False
+                break
+            
+            if n % m == 0:
+                a.append(n//m)
+            else:
+                status = False
+                # print('Boring!')
+                break
+
+            n //= m
+
+        if len(a) == 1:
+            status = False
         
+        if status:
+            # [最快寫法]直接印出 [125, 25, 5, 1]，結果為 125 25 5 1
+            print(*a)  
+            #原作者寫法
+            first = True
+            for n in a:
+                if first:
+                    print(n,end='')
+                    first = False
+                else:
+                    print(f' {n}', end='')
+            print()
+        else:
+            print('Boring!')
+    except EOFError:
+        break
+'''       
